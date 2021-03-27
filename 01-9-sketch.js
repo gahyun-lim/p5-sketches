0630d216a1866e76ad0e08f8c50978ab5fecbddd
gahyun-lim <78436934+gahyun-lim@users.noreply.github.com>
2021-03-27 03:41:09 -0400

    Add files via upload

---
+++ 01-9-sketch.js
@@ -0,0 +1,40 @@
+const sketchNumber = "20";
+
+let letterH;
+
+
+function preload () {
+    letterH = loadImage ('../assets/letterH.png')
+}
+
+function setup () {
+
+    createCanvas (1000, 900);
+    background ('white');
+    // image (letterH, 0, 0);
+
+    unitSize = 10;
+
+    for (i=0; i<height; i=i+unitSize) {
+        for (j=0; j<innerWidth; j=j+unitSize) {
+            pixelColor = letterH.get (j, i);
+
+            fill (pixelColor);
+            randomSize = random (20, 50);
+            ellipse (j, i, randomSize, randomSize);
+        }
+    }
+}
+
+// function draw () {
+    
+//     let pixelColor = get (mouseX, mouseY);
+
+//     console.log (pixelColor);
+// }
+
+
+
+function mousePressed () {
+    saveCanvas ("sketch-01", "png");
+}
